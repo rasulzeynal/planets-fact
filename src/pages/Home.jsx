7d1877f6cc6466191041ee929409ac755b1c378b
Rasul <rasul.zeynalov.95@gmail.com>
2023-02-01 00:42:33 +0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import "../style/home.scss";
+import earth from "../assets/planet-earth.svg";
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className='top'>
+        <div className='col-2 planet'>
+        <img src={earth} alt="planet"/>
+      </div>
+      <div className='col-4 info'>
+        <h1>EARTH</h1>
+        <p>Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.</p>
+        <p>Source <a>Wikipedia</a></p>
+        <button><h4>01</h4><h4>OVERVIEW</h4></button>
+        <button><h4>02</h4><h4>INTERNAL STRUCTURE</h4></button>
+        <button><h4>03</h4><h4>SURFACE GEOLOGY</h4></button>
+      </div>
+      </div>
+      <div className='statistic'>
+        <div className='rotation-time'>
+          <p>ROTATION TIME</p>
+          <h2>58.6 DAYS</h2>
+        </div>
+        <div className='revolution-time'>
+          <p>REVOLUTION TIME</p>
+          <h2>87.97 DAYS</h2>
+        </div>
+        <div className='radius'>
+          <p>RADIUS</p>
+          <h2>2,439.7 KM</h2>
+        </div>
+        <div className='average-temp'>
+          <p>AVERAGE TEMP.</p>
+          <h2>430°C</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
