4ae90633ffd2d13de7ff6f28ea2653a85c5297a1
Rasul <rasul.zeynalov.95@gmail.com>
2023-02-02 00:15:52 +0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='home container'>
       <div className='top d-flex'>
-        <div className='planet d-flex justify-content-center align-items-center col-8'>
+        <div className='planet d-flex justify-content-center align-items-center col-7'>
         <img src={earth} alt="planet"/>
       </div>
       <div className='info d-flex flex-column'>
@@ -14,14 +14,14 @@ const Home = () => {
         <p>Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.</p>
         <p>Source: <a href='https://en.wikipedia.org/wiki/Earth' target="_blank">Wikipedia</a></p>
         <div className="buttons d-flex flex-column mt-2">
-          <button><h4>01</h4><h4>OVERVIEW</h4></button>
-          <button><h4>02</h4><h4>INTERNAL STRUCTURE</h4></button>
-          <button><h4>03</h4><h4>SURFACE GEOLOGY</h4></button>
+          <button><h4>01</h4><h3>OVERVIEW</h3></button>
+          <button><h4>02</h4><h3>INTERNAL STRUCTURE</h3></button>
+          <button><h4>03</h4><h3>SURFACE GEOLOGY</h3></button>
         </div>
         
       </div>
       </div>
-      <div className='statistic'>
+      <div className='statistics d-flex mt-5 justify-content-between'>
         <div className='rotation-time'>
           <p>ROTATION TIME</p>
           <h2>58.6 DAYS</h2>
