3193d6a3b458c9aafec779c87aaa758f0f16e1fe
Rasul <rasul.zeynalov.95@gmail.com>
2023-02-02 09:16:37 +0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='planet d-flex justify-content-center align-items-center col-7'>
         <img src={earth} alt="planet"/>
       </div>
-      <div className='info d-flex flex-column'>
+      <div className='info d-flex flex-column col-4'>
         <h1>EARTH</h1>
         <p>Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.</p>
         <p>Source: <a href='https://en.wikipedia.org/wiki/Earth' target="_blank">Wikipedia</a></p>
